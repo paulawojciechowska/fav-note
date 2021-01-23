ba566ff5d2ecb947aa94affeabad1c79e4c4960f
astropaula <wojciechowska.fran@gmail.com>
2021-01-23 13:38:46 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="siema"
+      >
+        Learn React leioufhbW[EOGHNSDLKVBNDSLKDNFS,
+        MDNSDJVNS/sdjkgnspougb;eouithw;eignsldjgbs;doigb;
+        sdikngLDKVNS,DVNSLIDHVNLSM,NVSLDKNVFSLDKNVLSDKVLSODHNVndfslidns ;djnsubduflsinfid
+        spodojfjniv esidnf sifuni sij iosdfgj spodf vmsih
+        diljoiffslijfgsp9rugkjshdfgosihgolksndnlgi]
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
