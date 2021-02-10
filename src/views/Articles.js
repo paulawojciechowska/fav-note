2133c7d509bf2115a857829d1ebfb22fc8366467
astropaula <wojciechowska.fran@gmail.com>
2021-02-10 21:03:24 +0100

    Added Redux

--- src/views/Articles.js
+++ src/views/Articles.js
@@ -1,43 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import GridTemplate from 'templates/GridTemplate';
 import Card from 'components/molecules/Card/Card';
 
-const articles = [
-  {
-    id: 1,
-    title: 'React on my mind',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-    articleUrl: 'https://youtube.com/helloroman',
-    created: '1 day',
-  },
-  {
-    id: 2,
-    title: 'Wish you React',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-    articleUrl: 'https://youtube.com/helloroman',
-    created: '1 day',
-  },
-  {
-    id: 3,
-    title: 'You gave React a bad name',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-    articleUrl: 'https://youtube.com/helloroman',
-    created: '5 days',
-  },
-  {
-    id: 4,
-    title: 'Is it React you looking for?',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-    articleUrl: 'https://youtube.com/helloroman',
-    created: '10 days',
-  },
-];
-
-const Articles = () => (
+const Articles = ({ articles }) => (
   <GridTemplate sidebarType="articles">
     {articles.map(({ id, title, content, articleUrl, created }) => (
       <Card
@@ -53,4 +19,6 @@ const Articles = () => (
   </GridTemplate>
 );
 
-export default Articles;
+const mapStateToProps = ({ articles }) => ({ articles });
+
+export default connect(mapStateToProps)(Articles);
--- src/views/Twitter.js
+++ src/views/Twitter.js
@@ -1,43 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import GridTemplate from 'templates/GridTemplate';
 import Card from 'components/molecules/Card/Card';
 
-const twitters = [
-  {
-    id: 1,
-    title: 'Elon Musk',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-    created: '1 day',
-    twitterName: 'hello_roman',
-  },
-  {
-    id: 2,
-    title: 'Redux guy',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-    created: '1 day',
-    twitterName: 'dan_abramov',
-  },
-  {
-    id: 3,
-    title: 'React router stuff',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-    created: '5 days',
-    twitterName: 'mjackson',
-  },
-  {
-    id: 4,
-    title: 'Super animacje!',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-    created: '10 days',
-    twitterName: 'sarah_edo',
-  },
-];
-
-const Twitter = () => (
+const Twitter = ({ twitters }) => (
   <GridTemplate sidebarType="twitters">
     {twitters.map(({ id, title, content, created }) => (
       <Card
@@ -52,4 +19,22 @@ const Twitter = () => (
   </GridTemplate>
 );
 
-export default Twitter;
+Twitter.propTypes = {
+  twitters: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      cardType: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+      created: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+Twitter.defaultProps = {
+  twitters: [],
+};
+
+const mapStateToProps = ({ twitters }) => ({ twitters });
+
+export default connect(mapStateToProps)(Twitter);
